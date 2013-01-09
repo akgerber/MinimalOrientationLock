50a64a937209b3c73f3cce4956fb4aaaf1523159
Alan Gerber <me@alangerber.us>
2013-01-09 18:35:33 -0500

    Reorganized + cleaned code, moved action to WidgetProvider

---
+++ src/us/alangerber/minimalorientationlock/OrientationLockProvider.java
@@ -0,0 +1,96 @@
+package us.alangerber.minimalorientationlock;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+import android.util.Log;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+public class OrientationLockProvider extends AppWidgetProvider {
+	private static final String TAG = "RotationLockProvider";
+	public static final String INTENT_TOGGLE_ORIENTATION_LOCK =
+			"us.alangerber.minimalorientationlock.INTENT_TOGGLE_ORIENTATION_LOCK";
+	
+	/**
+	 * Handle incoming intents. If the intent 
+	 */
+	@Override
+	public void onReceive(Context ctx, Intent intent) {
+        if (intent.getAction().equals(INTENT_TOGGLE_ORIENTATION_LOCK)) {
+        	int appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
+                    AppWidgetManager.INVALID_APPWIDGET_ID);
+			try {
+				boolean result = toggleOrientationLock(ctx);
+				Toast.makeText(ctx, result? R.string.orientation_locked : R.string.orientation_unlocked,
+						Toast.LENGTH_SHORT).show();
+				setLockIcon(ctx, appWidgetId, result);
+			} catch (SettingNotFoundException e) {
+				//If the screen orientation setting isn't found, there's no way to recover.
+				Toast.makeText(ctx, R.string.orientation_missing, Toast.LENGTH_LONG).show();
+				Log.e(TAG, "Cannot access screen orientation setting :(");
+			}
+		} else {
+			super.onReceive(ctx, intent);
+		}
+	}
+
+	@Override
+	public void onUpdate(Context ctx, AppWidgetManager awm, int[] widgetIds) {
+		//iterate through widget instances
+		for (int widgetId : widgetIds) {
+			//Initialize a pending widget to tell this provider to lock/unlock the screen orientation setting
+			Intent mIntent = new Intent(ctx, OrientationLockProvider.class);
+			mIntent.setAction(INTENT_TOGGLE_ORIENTATION_LOCK);
+			mIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId);
+			PendingIntent mPendingIntent = PendingIntent.getBroadcast(ctx, 0, mIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+			
+			//Setup the widget UI, checking whether the rotation setting is set already
+			RemoteViews mRemoteViews = new RemoteViews(ctx.getPackageName(), R.layout.orientation_lock_button);
+			if (getOrientationLock(ctx)) {
+				mRemoteViews.setImageViewResource(R.id.lock_button, R.drawable.lock_open);
+			}
+			mRemoteViews.setOnClickPendingIntent(R.id.lock_button, mPendingIntent);
+			
+			awm.updateAppWidget(widgetId, mRemoteViews);
+
+		}
+		super.onUpdate(ctx, awm, widgetIds);
+	}
+	
+	private boolean toggleOrientationLock(Context ctx) throws SettingNotFoundException{
+		ContentResolver mContentResolver = ctx.getContentResolver();
+		if (Settings.System.getInt(mContentResolver, Settings.System.ACCELEROMETER_ROTATION) == 1){
+			Settings.System.putInt(mContentResolver, Settings.System.ACCELEROMETER_ROTATION, 0);
+			return true;
+		} else {
+			Settings.System.putInt(mContentResolver, Settings.System.ACCELEROMETER_ROTATION, 1);	
+			return false;
+		}
+	}
+	
+	private boolean getOrientationLock(Context ctx) {
+		ContentResolver mContentResolver = ctx.getContentResolver();
+		try {
+			return Settings.System.getInt(mContentResolver, Settings.System.ACCELEROMETER_ROTATION) == 1;
+		} catch (SettingNotFoundException e) {
+			return false;
+		}
+	}
+	
+	private void setLockIcon(Context ctx, int appWidgetId, boolean locked) {
+        AppWidgetManager mgr = AppWidgetManager.getInstance(ctx);
+		RemoteViews mRemoteViews = new RemoteViews(ctx.getPackageName(), R.layout.orientation_lock_button);
+		if (locked) {
+			mRemoteViews.setImageViewResource(R.id.lock_button, R.drawable.lock);
+		} else {
+			mRemoteViews.setImageViewResource(R.id.lock_button, R.drawable.lock_open);
+		}
+		mgr.updateAppWidget(appWidgetId, mRemoteViews);
+	}
+}
